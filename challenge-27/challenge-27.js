e7ce7586d53990edad500ce99ca36fe117044b78
Chrysthian Moizes <chrysthian.moizes@basis.com.br>
2019-04-03 15:17:00 -0300

    Desafio 27 e 28

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -19,3 +19,91 @@ Crie os seguintes métodos para verificação de tipo:
 - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
 O método isNull deve retornar `true` se o valor for null ou undefined.
 */
+
+(function() {
+    'use strict';
+
+    function DOM(elements) {
+      this.element = document.querySelectorAll(elements);
+    }
+
+    DOM.prototype.on = function on(eventType, callback) {
+      Array.prototype.forEach.call(this.element, function(element) {
+        element.addEventListener(eventType, callback, false);
+      });
+    };
+
+    DOM.prototype.off = function off(eventType, callback) {
+      Array.prototype.forEach.call(this.element, function(element) {
+        element.removeEventListener(eventType, callback, false);
+      });
+    }
+
+    DOM.prototype.get = function get() {
+      return this.element;
+    };
+
+    DOM.prototype.forEach = function forEach() {
+      return Array.prototype.forEach.apply( this.element, arguments );
+    };
+
+    DOM.prototype.map = function map() {
+      return Array.prototype.map.apply( this.element, arguments );
+    };
+
+    DOM.prototype.filter = function filter() {
+      return Array.prototype.filter.apply( this.element, arguments );
+    };
+
+    DOM.prototype.reduce = function reduce() {
+      return Array.prototype.reduce.apply( this.element, arguments );
+    };
+
+    DOM.prototype.reduceRight = function reduceRight() {
+      return Array.prototype.reduceRight.apply( this.element, arguments );
+    };
+
+    DOM.prototype.every = function every() {
+      return Array.prototype.every.apply( this.element, arguments );
+    };
+
+    DOM.prototype.some = function some() {
+      return Array.prototype.some.apply( this.element, arguments );
+    };
+
+    var $a = new DOM('[data-js="link"]');
+    console.log($a);
+
+    function is(obj) {
+        return Object.prototype.toString.call(obj);
+    }
+
+    DOM.prototype.isArray = function isArray(obj) {
+        return is(obj) === '[object Array]';
+    }
+
+    DOM.prototype.isObject = function isObject(obj) {
+        return is(obj) === '[objet Object]';
+    }
+
+    DOM.prototype.isFunction = function isFunction(obj) {
+        return is(obj) === '[object Function]';
+    }
+
+    DOM.prototype.isNumber = function isNumber(obj) {
+        return is(obj) === '[object Number]';
+    }
+
+    DOM.prototype.isString = function isString(obj) {
+        return is(obj) === '[object String]';
+    }
+
+    DOM.prototype.isBoolean = function isBoolean(obj) {
+        return is(obj) === '[object Boolean]';
+    }
+
+    DOM.prototype.isNull = function isNull(obj) {
+        return obj === null || obj === undefined;
+    }
+
+})();
--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,3 +1,7 @@
+(function () {
+
+  'use strict';
+
   /*
   No HTML:
   - Crie um formulário com um input de texto que receberá um CEP e um botão
@@ -25,3 +29,170 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+ //http://apps.widenet.com.br/busca-cep/api-de-consulta
+
+  function DOM(elements) {
+    this.element = document.querySelectorAll(elements);
+  }
+
+  DOM.prototype.on = function on(eventType, callback) {
+    Array.prototype.forEach.call(this.element, function(element) {
+      element.addEventListener(eventType, callback, false);
+    });
+  };
+
+  DOM.prototype.off = function off(eventType, callback) {
+    Array.prototype.forEach.call(this.element, function(element) {
+      element.removeEventListener(eventType, callback, false);
+    });
+  }
+
+  DOM.prototype.get = function get() {
+    return this.element;
+  };
+
+  DOM.prototype.forEach = function forEach() {
+    return Array.prototype.forEach.apply( this.element, arguments );
+  };
+
+  DOM.prototype.map = function map() {
+    return Array.prototype.map.apply( this.element, arguments );
+  };
+
+  DOM.prototype.filter = function filter() {
+    return Array.prototype.filter.apply( this.element, arguments );
+  };
+
+  DOM.prototype.reduce = function reduce() {
+    return Array.prototype.reduce.apply( this.element, arguments );
+  };
+
+  DOM.prototype.reduceRight = function reduceRight() {
+    return Array.prototype.reduceRight.apply( this.element, arguments );
+  };
+
+  DOM.prototype.every = function every() {
+    return Array.prototype.every.apply( this.element, arguments );
+  };
+
+  DOM.prototype.some = function some() {
+    return Array.prototype.some.apply( this.element, arguments );
+  };
+
+  var $a = new DOM('[data-js="link"]');
+  console.log($a);
+
+  function is(obj) {
+      return Object.prototype.toString.call(obj);
+  }
+
+  DOM.prototype.isArray = function isArray(obj) {
+      return is(obj) === '[object Array]';
+  }
+
+  DOM.prototype.isObject = function isObject(obj) {
+      return is(obj) === '[objet Object]';
+  }
+
+  DOM.prototype.isFunction = function isFunction(obj) {
+      return is(obj) === '[object Function]';
+  }
+
+  DOM.prototype.isNumber = function isNumber(obj) {
+      return is(obj) === '[object Number]';
+  }
+
+  DOM.prototype.isString = function isString(obj) {
+      return is(obj) === '[object String]';
+  }
+
+  DOM.prototype.isBoolean = function isBoolean(obj) {
+      return is(obj) === '[object Boolean]';
+  }
+
+  DOM.prototype.isNull = function isNull(obj) {
+      return obj === null || obj === undefined;
+  }
+
+  var $logradouro = new DOM('[data-js="logradouro"]');
+  var $bairro = new DOM('[data-js="bairro"]');
+  var $estado = new DOM('[data-js="estado"]');
+  var $cidade = new DOM('[data-js="cidade"]');
+  var $cep = new DOM('[data-js="cep"]');
+  var $status = new DOM('[data-js="status"]');
+
+  var $formCep = new DOM('[data-js="form-cep"');
+  var $inputCep = new DOM('[data-js="input-cep"]');
+  $formCep.on('submit', handleSubmitFormCEP);
+  var ajax = new XMLHttpRequest();
+
+  function handleSubmitFormCEP(event) {
+    event.preventDefault();
+    var url = getUrl();
+    ajax.open('GET', url);
+    ajax.send();
+    getMessage('loading');
+    ajax.addEventListener('readystatechange', handleReadyStateChange);
+  }
+
+  function getUrl() {
+    return 'https://viacep.com.br/ws/[CEP]/json'.replace('[CEP]',
+      clearCEP());
+  }
+
+  function handleReadyStateChange() {
+    if(isRequestOk()){
+      getMessage('ok');
+      fillCEPFields();
+    }
+  }
+
+  function fillCEPFields() {
+    var data = parseData();
+
+    if(!data)
+      return getMessage('error');
+
+    $logradouro.get()[0].textContent = data.logradouro;
+    $bairro.get()[0].textContent = data.bairro;
+    $estado.get()[0].textContent = data.uf;
+    $cidade.get()[0].textContent = data.localidade;
+    $cep.get()[0].textContent = data.cep;
+  }
+
+  function isRequestOk() {
+    return ajax.readyState === 4 && ajax.status === 200;
+  }
+
+  function parseData() {
+    var result = null;
+
+    try {
+      result = JSON.parse(ajax.responseText);
+    } catch (e) {
+      result = null;
+    }
+    return result;
+  }
+
+  function clearCEP() {
+    return $inputCep.get()[0].value.replace(/\D/g, '');
+  }
+
+  function getMessage(type) {
+
+    var cep = clearCEP();
+    var messages = {
+      loading: 'Buscando informações para o CEP []...',
+      ok: 'Endereço referente ao CEP []:',
+      error: 'Não foi possível encontrar o endereço para o CEP [].'
+    };
+    $status.get()[0].textContent = messages[type].replace('[]', cep);
+  }
+
+})();
+
+
+
+
